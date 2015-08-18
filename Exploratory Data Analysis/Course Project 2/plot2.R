f2bf0f7fdab2837ad94d0d33b6aaa081535ad101
Dave Bucholtz <dave@bucholtz.org>
2015-08-18 19:42:25 -0400

    Plot 2 and update to put a title in plot 1

--- Exploratory Data Analysis/Course Project 2/plot2.R
+++ Exploratory Data Analysis/Course Project 2/plot2.R
@@ -3,16 +3,17 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 ## Aggregate by the year to get totals and fix column names
-total.emissions <- aggregate(NEI[, "Emissions"], by = list(NEI[, "year"]), FUN = sum, na.rm = TRUE)
+NEI.baltimore <- NEI[NEI[, "fips"] == 24510, ]
+total.emissions <- aggregate(NEI.baltimore[, "Emissions"], by = list(NEI.baltimore[, "year"]), FUN = sum, na.rm = TRUE)
 colnames(total.emissions) <- c("Year", "TotalEmissions")
 
 ## Do this to make the scientific notation go away (our numbers aren't that big)
 options(scipen = 5)
 
 ## Plot a line chart and fit a line
-plot(total.emissions[, "Year"], total.emissions[, "TotalEmissions"], type = "l", xlab = "Year", ylab = "Total Emissions", main = "Trend in blue", scipen = 5)
+plot(total.emissions[, "Year"], total.emissions[, "TotalEmissions"], type = "l", xlab = "Year", ylab = "Total Emissions", main = "Trend for Baltimore in blue", scipen = 5)
 abline(lm(total.emissions[, "TotalEmissions"] ~ total.emissions[, "Year"]), col = "blue")
 
 #write the file plot1.png
-dev.copy(device = png, "plot1.png")
+dev.copy(device = png, "plot2.png")
 dev.off()
